f9509f2b8af478bdbf5b03b1bf8c14889cafb67c
pvyas033 <pankajvyas033@gmail.com>
2025-05-25 00:54:47 +0530

    changing test

--- tests/test_detect_lang.py
+++ tests/test_detect_lang.py
@@ -4,4 +4,4 @@ def test_detect_language_english():
     assert detect_language("French") == "en"
 
 def test_detect_language_hindi():
-    assert detect_language("नमस्ते") == "hi"
+    assert detect_language("नमस्ते") == "fr"
